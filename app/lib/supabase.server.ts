b85fcb92725e473d9a62c57ee4a5e600354f342d
Əli Əliyev <ali.aliyev@optima.az>
2025-10-25 16:27:49 +0400

    secrets fixing

--- app/lib/supabase.server.ts
+++ app/lib/supabase.server.ts
@@ -5,8 +5,8 @@ export function createClient(request: Request) {
   const headers = new Headers();
 
   const supabase = createServerClient<Database>(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
+    process.env.VITE_SUPABASE_URL!,
+    process.env.VITE_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
